3c6b50c136fb5b2b4e231d0ba7643631d74f1985
박준호 <107102057+wnsgh06@users.noreply.github.com>
2022-06-29 21:03:40 +0900

    Delete Web scraper directory

--- Web scraper/save.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def save_to_file(jobs):
-    file = open("jobs.csv", mode="w", newline='', encoding='utf-8')
-    writer = csv.writer(file)
-    writer.writerow(["title", "company", "location", "link"])
-    for job in jobs:
-        writer.writerow(list(job.values()))
-    return
